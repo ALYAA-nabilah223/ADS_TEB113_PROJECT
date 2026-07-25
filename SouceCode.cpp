470b4cb35b4ad1f3461f2b802dc541cb4f901972
Nur alyaa Nabilah <cfirst489@gmail.com>
2026-07-25 21:11:17 +0800

    Added Borrow Function (Base vs Optimized)

--- SouceCode.cpp
+++ SouceCode.cpp
@@ -20,7 +20,7 @@ void loadBooksFromFile() {
     //check if book file exixst
     ifstream file("books.txt");
     if (!file.is_open()) {
-        cout << "ERROR: books.txt not found!\n";
+        cout<<"ERROR: books.txt not found!\n";
         return;
     }
 
@@ -55,15 +55,46 @@ void loadBooksFromFile() {
         bookIds.push_back(id);
     }
     file.close();
-    cout << "Loaded " << count << " books from books.txt\n";
+    cout<<"Loaded "<<count<<" books from books.txt\n";
+}
+
+//Borrow: Base Function O(n)
+void baseline_borrow(string book_id) {
+    int steps = 0;
+    cout<<"\n1. BASELINE (O(n))\n";
+    for (string id : bookIds) {
+        steps++;
+        if (id == book_id) {
+            if (catalog[id].available_copies > 0) {
+                catalog[id].available_copies--;
+                cout<<"Successfully Borrowed! (Scanned "<<steps<<" books)\n";
+            } else {
+                cout<<"Uhh, Book is Unavailable!!. (Scanned"<<steps<<" books)\n";
+            }
+            break;
+        }
+    }
+    
+}
+
+//Borrow: Optimized Function O(1)
+void optimized_borrow(string student_id, string book_id) {
+    cout<<"\n2. OPTIMIZED (O(1))\n";
+    Book &book = catalog[book_id];
+    
+    if (book.available_copies > 0) {
+        book.available_copies--;
+        cout<<"SuccessfullyBorrowed! (1 lookup)\n";
+    } else {
+        book.waitlist.push(student_id);
+        cout<<"Successfully added "<<student_id<<" to queue. (Pos: "<< book.waitlist.size()<<")\n";
+    }
 }
 
 int main() {
-    cout << "Testing file loader\n";
+    cout<<"Testing file loader\n";
     loadBooksFromFile();
-
-    cout << "Total books in catalog: " << catalog.size() << "\n";
-    cout << "Total book IDs in vector: " << bookIds.size() << "\n";
-
+    cout<<"Total books in catalog: "<<catalog.size()<<"\n";
+    cout<<"Total book IDs in vector: "<<bookIds.size()<<"\n";
     return 0;
 }
